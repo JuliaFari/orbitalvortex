d61d44d7d9e988a814435303cdf9371c5847cc1b
Júlia Farias <juliafariasoliveira18@gmail.com>
2025-05-21 14:52:08 -0300

    Delete orbital-vortex directory

--- orbital-vortex/src/index.js
+++
@@ -1,9 +0,0 @@
-// Entry point for your JavaScript application
-// Example: render a React component into the root div
-
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
