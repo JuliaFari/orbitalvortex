4957559ea4f0be817ade1f869c273ad5737789bc
Júlia Farias <juliafariasoliveira18@gmail.com>
2025-05-21 14:52:42 -0300

    Add files via upload

---
+++ src/pages/Login.js
@@ -0,0 +1,29 @@
+// src/pages/Login.js
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const [code, setCode] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (code.startsWith("OV-ADM")) {
+      navigate("/teacher"); // Professor
+    } else if (code.startsWith("OV-ALUNO")) {
+      navigate("/student"); // Aluno
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Insira seu código"
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+      />
+      <button type="submit">Entrar</button>
+    </form>
+  );
+}
