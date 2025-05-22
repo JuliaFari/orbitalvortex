939a5e4b6e1eb6bc2bf075c243ac72698b8c61b4
Júlia Farias <juliafariasoliveira18@gmail.com>
2025-05-21 23:29:17 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,3 @@
-// src/App.js
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Student from "./pages/Student";
-import Teacher from "./pages/Teacher";
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
@@ -19,20 +12,6 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/student" element={<Student />} />
-        <Route path="/teacher" element={<Teacher />} />
-      </Routes>
-    </Router>
-  );
-}
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/aluno" element={<Student />} />
         <Route path="/professor" element={<Teacher />} />
       </Routes>
     </BrowserRouter>
